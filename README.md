3b04bcf09f4ad6c2693de88326fdb41e429af49c
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2022-11-30 17:15:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Umwandlung VFLDBS-Excelausgabe in CSV-Dateien
+
+### TODO
+* Nachforschung: Wie soll der Vegetationszyklus sich auf das gespeicherte Jahr im Zielformat auswirken? 
---
+++ vfl2csv/TrialSiteSheet.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+import openpyxl
+import pandas as pd
+
+
+class TrialSiteSheet:
+    def __init__(self, filepath: Path, sheet_name: str):
+        self.filepath = filepath
+        self.sheet_name = sheet_name
+        self.metadata = self.parse_metadata()
+        self.data = self.parse_data()
+
+    def parse_metadata(self):
+        """
+        Parse the metadata from the Excel file.
+        :return: Dict
+        """
+        # open excel file directly to extract metadata
+        excel_file = openpyxl.load_workbook(filename=self.filepath, read_only=True)
+        sheet = excel_file[self.sheet_name]
+
+        # extract metadata saved in the columns A5:A11 in a 'key : value' format
+        metadata = dict()
+        for row in sheet.iter_rows(min_row=5, max_row=11, min_col=1, max_col=1, values_only=True):
+            key, value = row[0].split(':')
+            metadata[key.strip()] = value.strip()
+        return metadata
+
+    def parse_data(self):
+        """
+        Parse the measurement data from the Excel file and return it in the target format.
+        :return: DataFrame
+        """
+        # Read the file again, this time using pandas to create dataframe directly.
+        # Everything before row 14 is only metadata that is being handled in a different function.
+        df = pd.read_excel(self.filepath, sheet_name=self.sheet_name, header=list(range(0, 4)), skiprows=13)
+        '''
+        Rename columns: The first three columns provide the tree population, tree species and tree id, then measurements follow.
+        For each measurement recording, there are three columns:
+         1. D: Durchmesser / diameter
+         2. Aus: Ausscheidungskennung / reason why a tree ceased to stand in the trial site
+         3. H: Höhe / height
+        
+        Turns out, there are four header rows:
+         1. date on which measurements where taken
+         2. type of measurement (see above)
+         3. unit of the measurement
+         4. number of measurements
+        The 3. and 4. row are not important, the 1. and 2. form together the new column name in the format 'YYYY-XYZ'
+        where YYYY is the year when the measurements where taken and XYZ is the type of measurement, so one of D, Aus and H. 
+        
+        PyCharm reports a wrong type missmatch, so let's supress that:
+        '''
+        # noinspection PyTypeChecker
+        df.columns = (
+            'Bestandeseinheit',
+            'Baumart',
+            'Baumnummer'
+            * [f'{multi_index[1]}_{multi_index[0].year}' for multi_index in self.data.columns[3:]]
+        )
+
+        # Also, the first column 'Bestandeseinheit' / tree population id is not actually needed, so let's remove it
+        df = df.drop(columns='Bestandeseinheit')
+        return df
