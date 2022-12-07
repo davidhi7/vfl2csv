0563e8ed080662e800799b9f1d22d0c4e0759d29
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2022-12-07 20:14:02 +0100

    Basic multiprocessing

--- vfl2csv/__main__.py
+++ vfl2csv/__main__.py
@@ -3,13 +3,30 @@ import os
 import sys
 import logging
 from pathlib import Path
+from multiprocessing import Pool, get_context
+import numpy as np
 
 import openpyxl
 
 from TrialSiteSheet import TrialSiteSheet
 
-logging.basicConfig(format='%(levelname)s %(module)s: %(message)s', level=logging.INFO)
+SHEETS_PER_PROCESS = 32
+
+logging.basicConfig(format='%(levelname)s %(name)s: %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+def worksheet_pipeline(sheet_batch, workbook, in_mem_file, output_dir, process_index):
+    logger = logging.getLogger(str(process_index))
+    for sheet_name in sheet_batch:
+        logger.info(f'Converting sheet {sheet_name}...')
+        trialSiteSheet = TrialSiteSheet(workbook, in_mem_file, sheet_name)
+        # trial_site_folder = Path(trialSiteSheet.replace_metadata_keys(str(output_dir / '{versuch}_{teilfläche}')))
+        trial_site_folder = Path(output_dir / sheet_name)
+        os.makedirs(trial_site_folder, exist_ok=True)
+        trialSiteSheet.write_data(trial_site_folder / 'data.csv')
+        trialSiteSheet.write_metadata(trial_site_folder / 'metadata.txt')
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
@@ -30,12 +47,17 @@ if __name__ == '__main__':
     sheet_names = workbook.sheetnames
     logger.info(f'Found {len(sheet_names)} trial sites within the Excel file')
 
-    for sheet_name in sheet_names:
-        logger.info(f'Converting sheet {sheet_name}...')
-        trialSiteSheet = TrialSiteSheet(workbook, in_mem_file, sheet_name)
-        trial_site_folder = Path(trialSiteSheet.replace_metadata_keys(str(output_dir / '{versuch}_{teilfläche}')))
-        os.makedirs(trial_site_folder, exist_ok=True)
-        trialSiteSheet.write_data(trial_site_folder / 'data.csv')
-        trialSiteSheet.write_metadata(trial_site_folder / 'metadata.txt')
+    # use multiprocessing for improved performance
+    process_count = max(round(len(sheet_names) / SHEETS_PER_PROCESS), 1)
+    logger.info(f'Found {os.cpu_count()} CPU threads, {process_count} processes will be used')
+    with get_context('fork').Pool(processes=os.cpu_count()) as pool:
+        process_args = zip(
+            np.array_split(sheet_names, process_count),
+            process_count * [workbook],
+            process_count * [in_mem_file],
+            process_count * [output_dir],
+            range(process_count)
+        )
+        pool.starmap(worksheet_pipeline, process_args)
 
-    print('done')
+    logger.info('done')
