bc7d681afaa802c305f2851e36c2c58266ab10ca
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2023-01-18 16:00:02 +0100

    Remove unneeded imports

--- vfl2csv_forms/gui/GraphicalUI.py
+++ vfl2csv_forms/gui/GraphicalUI.py
@@ -3,9 +3,8 @@ import traceback
 from pathlib import Path
 
 from PySide6.QtCore import Qt, Slot, QSize, Signal
-from PySide6.QtGui import QScreen
 from PySide6.QtWidgets import QLabel, QWidget, QVBoxLayout, QPushButton, QHBoxLayout, QTableWidget, \
-    QAbstractItemView, QHeaderView, QMessageBox, QFileDialog, QTableWidgetItem, QProgressBar, QApplication
+    QAbstractItemView, QHeaderView, QMessageBox, QFileDialog, QTableWidgetItem, QProgressBar
 
 from vfl2csv_forms import config
 from vfl2csv_forms.gui.InputHandler import InputHandler
