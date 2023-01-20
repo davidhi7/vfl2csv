8a85ecbb51155eef980591ea50cfd43cfa446607
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2023-01-20 15:33:10 +0100

    Update form progress display

--- vfl2csv_forms/gui/GraphicalUI.py
+++ vfl2csv_forms/gui/GraphicalUI.py
@@ -134,7 +134,7 @@ class GraphicalUI(QWidget):
 
         self.progress_bar.setMinimum(0)
         # one step each to write the df and then apply formatting and metadata
-        self.progress_bar.setMaximum(2 * len(self.input_handler))
+        self.progress_bar.setMaximum(len(self.input_handler))
         self.progress_bar.setValue(0)
         self.progress_bar.setVisible(True)
         self.manage_space()
