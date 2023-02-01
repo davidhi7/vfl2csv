0e05630db24a965d31ff04f7ac630a327c45429a
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2023-02-01 15:07:15 +0100

    Refactor argument parser into separate file

--- vfl2csv/__main__.py
+++ vfl2csv/__main__.py
@@ -3,22 +3,10 @@ import logging
 import pathlib
 
 from vfl2csv import batch_converter
+from vfl2csv.input.ArgumentParser import parser
 
 logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        prog='vfl2csv',
-        description='Convert Versuchsflächendatenbanksystem-output into CSV files.'
-    )
-    parser.add_argument('output',
-                        action='store',
-                        type=pathlib.Path,
-                        help='The output directory')
-    parser.add_argument('input',
-                        action='store',
-                        nargs='+',
-                        type=pathlib.Path,
-                        help='One or multiple input files or directories')
     arguments = vars(parser.parse_args())
     batch_converter.run(arguments['output'], arguments['input'])
