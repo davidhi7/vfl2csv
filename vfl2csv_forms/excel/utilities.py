5e7dec87ce9407bae1791db402bea1bd3281b5f8
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2023-02-03 21:52:13 +0100

    Add function to generate Excel cell names

--- vfl2csv_forms/excel/utilities.py
+++ vfl2csv_forms/excel/utilities.py
@@ -4,3 +4,7 @@ EXCEL_COLUMN_NAMES = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def zero_based_cell(ws, row, column):
     return ws.cell(row=row + 1, column=column + 1)
+
+
+def zero_based_cell_name(column, row) -> str:
+    return f'{EXCEL_COLUMN_NAMES[column + 1]}{row + 1}'
