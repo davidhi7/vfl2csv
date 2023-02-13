48c55fc8ecee1b78149248a28d872904c2eeb264
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2023-02-13 16:06:22 +0100

    Add `zero-based-cell-range`

--- vfl2csv_forms/excel/utilities.py
+++ vfl2csv_forms/excel/utilities.py
@@ -6,6 +6,10 @@ def zero_based_cell(ws, column, row):
     return ws.cell(row=row + 1, column=column + 1)
 
 
+def zero_based_cell_range(ws, column0, row0, column1, row1):
+    return ws[zero_based_cell_range_name(column0, row0, column1, row1)]
+
+
 def zero_based_cell_name(column, row) -> str:
     return f'{EXCEL_COLUMN_NAMES[column]}{row + 1}'
 
