c3a74680732bc852d965e01b1bb4c7977a324bbd
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2023-02-13 17:44:33 +0100

    Add function for estimating the character length of a string or number

--- vfl2csv_forms/excel/utilities.py
+++ vfl2csv_forms/excel/utilities.py
@@ -1,3 +1,5 @@
+import math
+
 # noinspection SpellCheckingInspection
 EXCEL_COLUMN_NAMES = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
@@ -16,3 +18,15 @@ def zero_based_cell_name(column, row) -> str:
 
 def zero_based_cell_range_name(column0, row0, column1, row1) -> str:
     return f'{EXCEL_COLUMN_NAMES[column0]}{row0 + 1}:{EXCEL_COLUMN_NAMES[column1]}{row1 + 1}'
+
+
+def get_character_count(value: str | int | float, decimal_digits: int) -> int:
+    if isinstance(value, str):
+        return len(value)
+    elif isinstance(value, int):
+        return int(math.log10(value))
+    elif isinstance(value, float):
+        # count of digits before the decimal point + the decimal points and count of displayed decimal digits
+        return int(math.log10(value)) + 1 + decimal_digits
+    else:
+        raise ValueError('Given value must be of type `int`, `float` or `string`!')
