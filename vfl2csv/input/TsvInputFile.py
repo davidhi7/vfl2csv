c022dbe217777ac52fad36a58dd1a014f8c7a7cd
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2023-03-02 11:18:27 +0100

    Make skiprows match the actual count of rows to skip, which is 14 in TSV files compared to 13 in Excel files

--- vfl2csv/input/TsvInputFile.py
+++ vfl2csv/input/TsvInputFile.py
@@ -33,7 +33,7 @@ class TsvInputFile(InputFile):
         # move back to start of the stream
         file_stream.seek(0)
 
-        df = pd.read_csv(file_stream, sep='\t', skiprows=13, header=list(range(0, 4)), decimal=',')
+        df = pd.read_csv(file_stream, sep='\t', skiprows=14, header=list(range(0, 4)), decimal=',')
         # this last column is only created by pandas because in the output format, each row ends with one tabulator
         # instead of the last value. Consequently, this last column does not contain any values and needs to be removed.
         df = df.drop(columns=df.columns[-1])
