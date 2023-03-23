29526273c1916f6311505d6bb91814676d512387
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2023-03-23 17:50:16 +0100

    Fix error handling

--- vfl2csv_gui/subsystems/forms/FormsConversionWorker.py
+++ vfl2csv_gui/subsystems/forms/FormsConversionWorker.py
@@ -9,6 +9,7 @@ from vfl2csv_base.TrialSite import TrialSite
 from vfl2csv_forms import trial_site_conversion
 from vfl2csv_forms.excel import styles
 from vfl2csv_gui.interfaces.CommunicationSignals import CommunicationSignals
+from vfl2csv_gui.interfaces.ExceptionReport import ExceptionReport
 
 logger = logging.getLogger(__name__)
 
@@ -24,19 +25,17 @@ class ConversionWorker(QRunnable):
     def run(self) -> None:
         self.output_file.touch(exist_ok=True)
 
-        trial_site_forms = []
         with ExcelWriter(self.output_file, engine='openpyxl') as writer:
             try:
                 workbook = writer.book
                 styles.register(workbook)
                 for trial_site in self.trial_sites:
                     trial_site_form = trial_site_conversion.convert(trial_site, self.output_file)
-                    trial_site_forms.append(trial_site_form)
                     trial_site_form.init_worksheet(writer)
                     trial_site_form.create(workbook)
                     self.signals.progress.emit(str(trial_site))
-            except Exception as err:
-                logger.error(err)
-                logger.error(traceback.format_exc())
-                self.signals.error.emit(err)
+            except Exception as exc:
+                exc_traceback = traceback.format_exc()
+                logger.warning(str(exc) + '\n' + exc_traceback)
+                self.signals.error.emit(ExceptionReport(exc, exc_traceback))
         self.signals.finished.emit()
