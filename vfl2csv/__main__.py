8d8a2da1bf3614aab11540c2eb4038b63a7f7182
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2023-03-27 11:49:50 +0200

    Allow running vfl2csv CLI from vfl2csv_gui

--- vfl2csv/__main__.py
+++ vfl2csv/__main__.py
@@ -7,14 +7,19 @@ from vfl2csv.batch_converter import run
 
 logger = logging.getLogger(__name__)
 
-if __name__ == '__main__':
-    arguments = vars(parser.parse_args())
+
+def start(args):
+    arguments = vars(parser.parse_args(args))
     if 'config' in arguments or 'column_scheme' in arguments:
         vfl2csv.set_custom_configs(config_path=arguments['config'], column_scheme_path=arguments['column_scheme'])
     success, report = run(arguments['output'], arguments['input'], None)
     if not success:
         logger.info('Failed')
-        sys.exit(1)
+        return 1
 
     logger.info('Done')
-    sys.exit(0)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(start(sys.argv[1:]))
--- vfl2csv_gui/__main__.py
+++ vfl2csv_gui/__main__.py
@@ -1,10 +1,25 @@
+import argparse
 import sys
 
 from PySide6.QtWidgets import QApplication
 
+from vfl2csv.__main__ import start
 from vfl2csv_gui.MainWindow import MainWindow
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='vfl2csv_gui',
+        description='GUI frontend for vfl2csv file conversion and form generation'
+    )
+    parser.add_argument(
+        '--cli',
+        action='store_true',
+        help='run vfl2csv in CLI mode'
+    )
+    parsed_args, remaining_args = parser.parse_known_args()
+    if vars(parsed_args)['cli']:
+        sys.exit(start(remaining_args))
+
     app = QApplication(sys.argv)
     main_window = MainWindow()
     main_window.show()
